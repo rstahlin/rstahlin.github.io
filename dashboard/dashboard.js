982a08efebd3da14651d35533a1ecacee858deb6
Noah Marcus <Walcott@Noahs-MBP.home>
2020-11-28 17:50:15 -0500

    updating new dashboard to support all tabs

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -28,9 +28,72 @@ $( document ).ready(function() {
         $('#neighborhood-data').addClass('active');
         // set dashboard header
         $('#dashboard-header').text('Neighborhood Data');
+    });
 
-       
+    $('#neighborhood-maps').on('click', function(){
+        // reverse others
+        $('.data-pane').hide();
+        $('.nav-link').removeClass('active');
 
+       // display neighborhood data 
+       $('#neighborhood-maps-pane').show();
+       // make neightborhood nav active
+       $('#neighborhood-maps').addClass('active');
+       // set dashboard header
+       $('#dashboard-header').text('Neighborhood Maps');
+   });
 
+   $('#age-data').on('click', function(){
+        // reverse others
+        $('.data-pane').hide();
+        $('.nav-link').removeClass('active');
+
+        // display neighborhood data 
+        $('#age-data-pane').show();
+        // make neightborhood nav active
+        $('#age-data').addClass('active');
+        // set dashboard header
+        $('#dashboard-header').text('Age Data');
+        // set internal first tab to active
+        $('age-data-new-cases-tab').addClass('active');
+    });
+
+    $('#ward-data').on('click', function(){
+        // ward others
+        $('.data-pane').hide();
+        $('.nav-link').removeClass('active');
+
+        // display neighborhood data 
+        $('#ward-data-pane').show();
+        // make neightborhood nav active
+        $('#ward-data').addClass('active');
+        // set dashboard header
+        $('#dashboard-header').text('Ward Data');
+    });
+
+    $('#hospitalization-data').on('click', function(){
+        // ward others
+        $('.data-pane').hide();
+        $('.nav-link').removeClass('active');
+
+        // display neighborhood data 
+        $('#hospitalization-data-pane').show();
+        // make neightborhood nav active
+        $('#hospitalization-data').addClass('active');
+        // set dashboard header
+        $('#dashboard-header').text('Hospitilization Data');
+    });
+
+    $('#race-and-ethnicity-data').on('click', function(){
+        // ward others
+        $('.data-pane').hide();
+        $('.nav-link').removeClass('active');
+
+        // display neighborhood data 
+        $('#race-and-ethnicity-data-pane').show();
+        // make neightborhood nav active
+        $('#race-and-ethnicity-data').addClass('active');
+        // set dashboard header
+        $('#dashboard-header').text('Race and Ethnicity Data');
     });
 });
